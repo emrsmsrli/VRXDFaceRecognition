bbf2a6a628cde69ca9db632b786495dc1b863207
Emre <emrsmsrli1@gmail.com>
2018-05-19 15:09:03 +0300

    change frameId to id

--- app/src/main/java/tr/edu/iyte/vrxdfacerecognition/Frame.kt
+++ app/src/main/java/tr/edu/iyte/vrxdfacerecognition/Frame.kt
@@ -2,4 +2,4 @@ package tr.edu.iyte.vrxdfacerecognition
 
 import tr.edu.iyte.vrxd.api.data.Shape
 
-data class Frame(val frameId: Int, val shapes: MutableList<Shape>, var isReady: Boolean = false)
+data class Frame(val id: Int, val shapes: MutableList<Shape>, var isReady: Boolean = false)
